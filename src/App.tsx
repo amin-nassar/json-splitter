c07c2344dae0dad17411f3a9d6bd20a92e2b95b5
amin-nassar <amin.nassar.ce@gmail.com>
2025-06-04 03:11:15 +0300

    Added footer and changed toasts positioning

--- src/App.tsx
+++ src/App.tsx
@@ -5,10 +5,23 @@ import "./styles/globals.css";
 
 function App() {
   return (
-    <main className=" h-screen max-h-screen bg-background text-foreground p-4">
-      <Splitter />
-      <Toaster position="bottom-center" richColors theme="light" />
-    </main>
+    <>
+      <main className=" h-screen max-h-screen bg-background text-foreground p-4">
+        <Splitter />
+        <Toaster position="top-center" richColors theme="light" />
+      </main>
+      <footer className="fixed bottom-0 w-full text-center py-3 text-sm text-muted-foreground border-t shadow-inner bg-background">
+        Built with ❤ by{" "}
+        <a
+          href="https://www.linkedin.com/in/amin-m-nassar"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="inline-flex items-center gap-1 text-primary font-bold underline"
+        >
+          Amin Nassar
+        </a>
+      </footer>
+    </>
   );
 }
 
